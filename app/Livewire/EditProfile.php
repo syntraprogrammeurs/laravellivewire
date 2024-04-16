8cceb89d1bfea98a43a58d34d18d420b755815b1
Neverlanders <vanhoutte.tom@gmail.com>
2024-04-16 12:03:09 +0200

    validation finished

--- app/Livewire/EditProfile.php
+++ app/Livewire/EditProfile.php
@@ -4,20 +4,20 @@ namespace App\Livewire;
 
 use App\Livewire\Forms\ProfileForm;
 use App\Models\User;
+
 use Illuminate\Validation\Rule;
 use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class EditProfile extends Component
 {
-    public ProFileForm $form;
+    public ProfileForm $form;
     public $showSuccess = false;
+
+
     public function mount(){
-        //$this->user = User::whereId(1)->first();
-       // $this->user = User::where('id',1)->first();
         $this->form->setUser(User::where('id',1)->first());
     }
-
     public function save(){
         $this->form->update();
         $this->showSuccess=true;
--- app/Livewire/Forms/ProfileForm.php
+++ app/Livewire/Forms/ProfileForm.php
@@ -10,44 +10,40 @@ use Livewire\Form;
 class ProfileForm extends Form
 {
     //
-    #[Validate]
     public User $user;
+    #[Validate]
     public $name='';
     public $content='';
-    public $emails=false;
+    public $emails = false;
     public $newsletter = false;
     public $marketing = false;
-    public $country='';
-
-    public $gender = false;
+    public $country = '';
 
     public function rules(){
         return[
             'name'=>[
                 'required',
                 Rule::unique('users')->ignore($this->user),
-            ],
+            ]
         ];
     }
     public function setUser(User $user){
+
         $this->user = $user;
         $this->name =$this->user->name;
         $this->content =$this->user->content;
-        $this->emails=$this->user->emails;
-        $this->newsletter=$this->user->newsletter;
-        $this->marketing=$this->user->marketing;
-        $this->country=$this->user->country;
-        $this->gender=$this->user->gender;
-
+        $this->emails= $this->user->emails;
+        $this->newsletter= $this->user->newsletter;
+        $this->marketing= $this->user->marketing;
+        $this->country= $this->user->country;
     }
     public function update(){
         $this->validate();
         $this->user->name = $this->name; $this->user->content = $this->content;
-        $this->user->emails= $this->emails;
-        $this->user->newsletter= $this->newsletter;
-        $this->user->marketing= $this->marketing;
-        $this->user->country= $this->country;
-        $this->user->gender= $this->gender;
+        $this->user->emails = $this->emails;
+        $this->user->newsletter = $this->newsletter;
+        $this->user->marketing = $this->marketing;
+        $this->user->country = $this->country;
         $this->user->save();
     }
 }
